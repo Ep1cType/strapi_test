523a1733d0c490ac1a4bca708c78fb5b7eb9c0ee
Ep1cType <DenRus38@hotmail.com>
2023-04-17 14:33:29 +0700

    feat: rangs

---
+++ src/api/rang/controllers/rang.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * rang controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::rang.rang');
---
+++ src/api/rang/routes/rang.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * rang router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::rang.rang');
